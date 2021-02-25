9a66d9f3da83fd549d311e69de9f9e6ce0a034fd
SankalpSwami <sankalpswami0102@gmail.com>
2021-02-25 12:08:38 +0530

    Amazon-Clone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; 
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="app">
+      <Switch>
+        <Route path="/checkout">
+        <Header />
+          <h1>Checkout Page</h1>
+        </Route>
+        <Route path="/login">
+        <Header />
+          <h1>Login Page</h1>
+        </Route>
+        <Route path="/">
+        <Header />
+          <Home />
+        </Route>
+      </Switch>      
     </div>
+    </Router>
   );
+
 }
+
+  {
+    /* WE NEED A REACT-ROUTER */
+  }
+
+  {
+    /* localhost.com */
+  }
+      
+  {
+    /* localhost.com/checkout */
+  }
+      
+  {
+    /* localhost.com/login */
+  }
 
 export default App;
